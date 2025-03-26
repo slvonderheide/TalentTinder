ed90c3b83e4f3049c741e2fe3b887779ad5b12f0
slvonderheide <slvonderheide@gmail.com>
2025-03-26 11:22:33 -0400

    updated index.ts

--- index.ts
+++ index.ts
@@ -7,6 +7,6 @@ app.get('/', (_req, res) => {
   res.send('Server is running!');
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
