74aefcf10cd930f278ca34669d253db50b109b34
slvonderheide <slvonderheide@gmail.com>
2025-03-26 12:10:15 -0400

    working on port

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    host: true, // Expose on 0.0.0.0
+    port: process.env.PORT ? Number(process.env.PORT) : 5173, // Use PORT from Render or default to 5173
+    strictPort: true, // Ensures Vite fails if the port is unavailable
+  }
 });
