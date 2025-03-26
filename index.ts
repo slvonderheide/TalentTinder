8765c855d42749c880791d6779740883faae11c2
slvonderheide <slvonderheide@gmail.com>
2025-03-26 11:41:54 -0400

    working on port

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const PORT = parseInt(process.env.PORT as string, 10) || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (_req, res) => {
   res.send('Server is running!');
